9bbd0a1d93f4e9177671dbb95e1efddb28f3b889
sevmezabdullah <abdullah.sevmez@rapid.com.tr>
2024-01-22 08:42:57 +0300

    cleaned up

--- lib/providers/books_provider.dart
+++ lib/providers/books_provider.dart
@@ -5,21 +5,13 @@ import '../services/book_service.dart';
 
 class BookServiceProvider with ChangeNotifier {
   List<Book> _books = [];
-  bool _isLoading = false;
-
-  bool get isLoading => _isLoading;
 
   List<Book> get books => _books;
 
   Future<void> getBooks(String query) async {
     try {
-      _isLoading = true;
-      notifyListeners();
       final List<Book> books = await BookService().getBooks(query);
       _books = books;
-      notifyListeners();
-
-      _isLoading = false;
       notifyListeners();
     } catch (e) {
       throw Exception('Kitapları getirirken bir hata meydana geldi $e');
